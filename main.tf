d47d4de69f1510559f9c6195b2bc617bc985634c
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-01-29 21:50:53 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "learn-terraform-aws-v2"
+    workspaces {
+      name = "Week-2"
+    }
+  }
+}
+
+# Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "n-virginia"
+}
+
+ 
