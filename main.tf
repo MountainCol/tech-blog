62ca535c79dfff851139417257ba036c53332565
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-02-16 16:11:07 +0000

    choreS3Bucket

--- main.tf
+++ main.tf
@@ -35,24 +35,10 @@ variable "domain_name" {
 # Bucket Config
 
 resource "aws_s3_bucket" "tech-blog" {
-    bucket = "cloudtalent-blog-bucket1234"
+  bucket = "cloudtalent-blog-bucket1234"
 
-    Version: "2012-10-17",
-    Statement: {
-        Sid: "AllowCloudFrontServicePrincipalReadOnly",
-        Effect: "Allow",
-        Principal: {
-            Service: "cloudfront.amazonaws.com"
-        },
-        Action: "s3:GetObject",
-        Resource: "arn:aws:s3:::cloudtalent-blog-bucket1234/*",
-        Condition: {
-            StringEquals: {
-                AWS:SourceArn: "arn:aws:cloudfront::010438472413:distribution/E3V6ZPS5K8GAB9"
-            }
-        }
-    }
 }
+
 ######################################
 ## Route 53 and Certificate Manager ##
 ######################################
